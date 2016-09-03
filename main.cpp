928b3fd3517fd055b3593f4f895516d4dcc76689
Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>
2016-09-03 12:36:23 +0200

    Add defines for different setup functionalities like - Choosing the sink (SHM or UDP) - Choosing the format (BGR or RGB) - Choosing the linux video device (DEVICE #) - Choosing wether you you want to see the output or not (SHOW)
    Signed-off-by: Timo Korthals <tkorthals@cit-ec.uni-bielefeld.de>

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
-//#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-#define SHOW   true
-#define DEVICE 0
+#define SHOW
+#define DEVICE  0
+// #define BGR2RGB
+// #define SINK_UDP
+#define SINK_SHM
 
 int main(int argc, char *argv[]){
   VideoCapture cap;    // Camera
   VideoWriter writer;  // gstreamer
   cout << "Press ESC to quit" << endl;
-
+   cout << getBuildInformation() << endl;
+//    return 0;
   // CAMERA SETUP
   cap.open(DEVICE);
   if(!cap.isOpened()) {
@@ -25,12 +28,18 @@ int main(int argc, char *argv[]){
   const int height = cap.get(CAP_PROP_FRAME_HEIGHT);
   cout << "Capture camera with " << fps << " fps, " << width << "x" << height << " px" << endl;
 
-  // GSTREAMER SETUP
+  // GSTREAMER-1.0 SETUP
+#ifdef SINK_UDP
   writer.open("appsrc ! rtpvrawpay ! udpsink host=localhost port=5000", 0, fps, cv::Size(width, height), true);
+#endif
+#ifdef SINK_SHM
+  writer.open("appsrc ! shmsink socket-path=/tmp/foo sync=true wait-for-connection=false shm-size=10000000", 0, fps, cv::Size(width, height), true);
+#endif
   if (!writer.isOpened()) {
     cap.release();
-    cerr << "Can't create gstreamer writer" << endl;
-//    cout << getBuildInformation() << endl;
+    cerr << "Can't create gstreamer writer. Do you have the correct version installed?" << endl;
+    cerr << "Print out OpenCV build information" << endl;
+    cout << getBuildInformation() << endl;
     return -1;
   }
 
@@ -43,13 +52,18 @@ int main(int argc, char *argv[]){
       if (frame.empty()) {
         throw;
       }
+#ifdef BGR2RGB
+      cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+#endif
       writer << frame;
-//       cv::imshow("Source image", frame);
-      key = cv::waitKey(1);
+#ifdef SHOW
+      cv::imshow("Source image", frame);
+#endif
     } catch (...) {
       cout << "Something went wrong" << endl;
       break;
     }
+    key = cv::waitKey(1);
   }
 
   // CLEANUP
